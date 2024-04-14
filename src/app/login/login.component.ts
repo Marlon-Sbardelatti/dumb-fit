32be58c9748ae35b29bf7797b2e68166d2766d78
Marlon-Sbardelatti <hetzwga@gmail.com>
2024-04-14 14:12:05 -0300

    verify fields

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,52 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [RouterLink, FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+	selector: "app-login",
+	standalone: true,
+	imports: [RouterLink, FormsModule],
+	templateUrl: "./login.component.html",
+	styleUrl: "./login.component.css",
 })
 export class LoginComponent {
-  constructor(public router: Router) { }
+	constructor(public router: Router) {}
 
-  user = {
-    name: '',
-    password: ''
-  }
+	user = {
+		name: "",
+		password: "",
+	};
 
-  goToPage(pageName: string) {
-    localStorage.setItem("user", JSON.stringify(this.user));
-    this.router.navigate([`${pageName}`]);
-  }
+	goToPage(pageName: string) {
+		if (
+			this.user.name == "" ||
+			(this.user.name == undefined && this.user.password == "") ||
+			this.user.password == undefined
+		) {
+			alert("Preencha todos os campos");
+		} else if (this.user.name == "" || this.user.name == undefined) {
+			alert("Preencha o nome de usuário");
+		} else if (this.user.password == "" || this.user.password == undefined) {
+			alert("Preencha a senha");
+		} else {
+			localStorage.setItem("user", JSON.stringify(this.user));
+			this.router.navigate([`${pageName}`]);
+		}
+	}
 
+	verifyUser(user: { name: string; password: string }): string {
+		if (
+			user.name == "" ||
+			(user.name == undefined && user.password == "") ||
+			user.password == undefined
+		) {
+			return "Preencha todos os campos";
+		} else if (user.name == "" || user.name == undefined) {
+			return "Preencha o nome de usuário";
+		} else if (user.password == "" || user.password == undefined) {
+			return "Preencha a senha";
+		}
+
+		return "test";
+	}
 }
